fb35ffca2e417f7b67051d7c36dfd8df138b8083
unknown <rutuja.dukhande7@gmail.com>
2023-04-24 09:05:40 -0500

    managing from txt file, delete operation

--- API_call.py
+++ API_call.py
@@ -1,48 +1,98 @@
 from flask import Flask, request, jsonify#import Flask
 import requests
+from flask_cors import CORS
+import json
+import random
+import string
 
 app = Flask(__name__) #creating flask app
+CORS(app)
 
 #personal_data = {1: {'name': 'Omkar Gawade', 'gender':'female'}, 2:{'name': 'Rutuja', 'gender': 'female'}}
 
-personal_data = {
-                1:{'name':'Omkar Gawade', 'gender':'Male'}, 
-                2:{'name':'Rutuja Dukhande', 'gender':'Female'},
-                3:{'name':'Sam Dukhande', 'gender':'Female'},
-                 }
+# personal_data = {
+#                 1:{'name':'Omkar Gawade', 'gender':'Male'}, 
+#                 2:{'name':'Rutuja Dukhande', 'gender':'Female'},
+#                 3:{'name':'Sam Dukhande', 'gender':'Female'},
+#                  }
+personal_data = {}
+with open("C:/Users/Rutuja/Desktop/Projects/myReactApp/Backend-API/data.txt", "r") as e:
+    personal_data_read = e.read()
 
+personal_data = eval(personal_data_read)
 
-@app.route('/', methods = ['GET']) #creating first endpoint
+print(personal_data)
+
+    
+@app.route('/', methods = ['GET']) #creating main endpoint
 def main_page():
-    return personal_data[1]['name']
+    return personal_data
 
 
-@app.route('/<int:num>', methods=['GET'])
+@app.route('/<string:num>', methods=['GET']) #endpoint to view data based on key #Read
 def user_page(num):
     for key, value in personal_data.items():
         if key == num:
             return value
     return 'wrong data1'
 
-@app.route('/<string:name>', methods = ['GET']) #creating first endpoint
+@app.route('/<string:name>', methods = ['GET']) #endpoint to view data based on 'name' #Read
 def user_page1(name):
     for user_id, user_data in personal_data.items():
         if user_data['name'] == name:
             return user_data
     return 'wrong data'
 
-@app.route('/updateRecord') #creating first endpoint
+@app.route('/create', methods = ['POST']) #endpoint to creating new record  #Create
+def createRecord():
+    data = request.json
+    #new_id = max(personal_data.keys()) +1
+    def generateNewid():
+        sample_string = ""
+        for i in range(5):
+            sample_string += "".join(random.choice(string.ascii_lowercase))
+        return sample_string
+    
+    new_id = generateNewid()
+    
+    for key, items in personal_data.items():
+        if data['name'] in personal_data[key]['name']:
+            return "The data is already is existed"
+    personal_data[new_id] = {'name': data['name'], 'gender': data['gender']}
+    return jsonify({'message': 'Person added successfully'}, personal_data[new_id])
+    
+    #return jsonify(new_record), 201
+
+@app.route('/updateRecord', methods = ['POST', 'GET']) #endpoint to update the data 
 def updateRecord():
-    key = int(request.args.get('key'))
-    name = request.args.get('name')
-    subname = request.args.get('subname')
+    
+    jsonData = request.get_json()
+    key = jsonData['key']
+    password = int(jsonData['password'])
+    value = jsonData['value']
     if key in personal_data:
-        if name in personal_data[key]:
-                personal_data[key][name] = subname  
-    return jsonify(personal_data)
+                #return jsonify(password, personal_data[key]['gender'])
+                if password % 2 == 0 and personal_data[key]['gender'] == "Female":
+                    personal_data[key]['name'] = value 
+                    return "done"
+                elif password % 2 != 0 and personal_data[key]['gender'] == "Male":
+                    personal_data[key]['name'] = value 
+                    return "done"
+                else:
+                    return "Invalid"
+            
+    
+    #return jsonify(key, value, subvalue,password)
+
+@app.route('/delete', methods = ['POST'])
+def deleteRecord():
+    jsonData = request.get_json()
+    key = jsonData['key']
+    if key in personal_data:
+        del personal_data[key]
+        return "Deleted"
 
 
-    #return personal_data[key][value] == subvalue
-
+    
 if __name__ == '__main__':
     app.run(debug=True)
